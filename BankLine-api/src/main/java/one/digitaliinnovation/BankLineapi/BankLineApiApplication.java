7b06cf0bbab3e8ed2c38d9c4c40de91644537fb8
carlos738 <carlosasl135@hotmail.com>
2022-08-19 23:17:53 -0300

    api

---
+++ BankLine-api/src/main/java/one/digitaliinnovation/BankLineapi/BankLineApiApplication.java
@@ -0,0 +1,13 @@
+package one.digitaliinnovation.BankLineapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankLineApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankLineApiApplication.class, args);
+	}
+
+}
---
+++ BankLine-api/src/test/java/one/digitaliinnovation/BankLineapi/BankLineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitaliinnovation.BankLineapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankLineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
